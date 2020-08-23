43fe6984c5e48c012cccdbdf43c95485d9688af8
levinmk23 <levinmk23@gmail.com>
2020-08-23 22:29:10 +0300

    issue #1 refactoring 5

--- module-1-intro/src/main/java/HelloWorld.java
+++ module-1-intro/src/main/java/HelloWorld.java
@@ -2,11 +2,13 @@ public class HelloWorld {
 
     public int x = 7;
     public String s = "123";
+
     public int funcForTest(int param) {
         return param + 1;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello CI )))");
     }
 }
